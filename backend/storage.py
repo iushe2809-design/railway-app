bce66587fa81b0dcd10bbcab1f1f059f88fd9175
iushe2809-design <iushe.2809@gmail.com>
2026-07-29 16:52:04 +0530

    Update storage.py

--- backend/storage.py
+++ backend/storage.py
@@ -9,9 +9,9 @@ import requests
 logger = logging.getLogger(__name__)
 
 cloudinary.config(
-    cloud_name=os.getenv("difywe8y"),
-    api_key=os.getenv("335271489957736"),
-    api_secret=os.getenv("7U_pYaj9vKeHYTX-rus8qDh83H4"),
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True,
 )
 
